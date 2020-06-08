e3fc6983848b2479ae80235ea3dcf8238b546840
Luiz Gomes <luiz.gmes@gmail.com>
2020-06-08 13:06:54 -0300

    ATIVIDADE 2 - Correção inclusão novo carro  ALAMOFIRE

--- PersistenciaNetwork/AulaSemana2/Carangas/Carangas/REST.swift
+++ PersistenciaNetwork/AulaSemana2/Carangas/Carangas/REST.swift
@@ -50,13 +50,13 @@ final class REST {
         let urlFipe = "https://fipeapi.appspot.com/api/1/carros/marcas.json"
         
         AF.request(urlFipe).response { response in
-
-        do {
+            
+            do {
                 if response.data == nil {
                     onError(.noData)
                     return
                 }
-            
+                
                 if let error = response.error {
                     
                     if error.isSessionTaskError || error.isInvalidURLError {
@@ -70,16 +70,16 @@ final class REST {
                         onError(.responseStatusCode(code: error._code))
                     }
                 }
-            
+                
                 let brands = try JSONDecoder().decode([Brand].self, from: response.data!)
-                    onComplete(brands)
-                } catch is DecodingError {
-                    onError(.invalidJSON)
-                } catch {
-                    onError(.taskError(error: error))
-                }
-            
+                onComplete(brands)
+            } catch is DecodingError {
+                onError(.invalidJSON)
+            } catch {
+                onError(.taskError(error: error))
             }
+            
+        }
         
     }
     
@@ -141,37 +141,53 @@ final class REST {
         // o endpoint do servidor para update é: URL/id
         let urlString = basePath + "/" + (car._id ?? "")
         
-    
-        var httpMethod: HTTPMethod = .get
+        guard let url = URL(string: urlString) else {
+            onComplete(false)
+            return
+        }
+        var request = URLRequest(url: url)
+        var httpMethod: String = ""
         
         switch operation {
         case .delete:
-            httpMethod = HTTPMethod.delete
+            httpMethod = "DELETE"
         case .save:
-            httpMethod = HTTPMethod.post
+            httpMethod = "POST"
         case .update:
-            httpMethod = HTTPMethod.put
+            httpMethod = "PUT"
         }
-        
+        request.httpMethod = httpMethod
         
         // transformar objeto para um JSON, processo contrario do decoder -> Encoder
         guard let json = try? JSONEncoder().encode(car) else {
             onComplete(false)
             return
         }
+        request.httpBody = json
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         
-        
-        AF.request(urlString, method: httpMethod, parameters: json).response { response in
+        AF.request(request)
+            .response {response in
+                
+                if response.error == nil {
+                    
+                    guard let responseFinal = response.response else {
+                        onError(.noResponse)
+                        return
+                    }
+                    
+                    if responseFinal.statusCode == 200 {
+
+                        onComplete(true)
+                        
+                    } else {
+                        onError(.responseStatusCode(code: response.response!.statusCode))
+                    }
+                } else {
+                    onError(.taskError(error: response.error!))
+                }
             
-            if response.error != nil {
-                onComplete(false)
-                return
             }
-            
-            onComplete(true)
-            
-        }
-            
     }
     
 } // fim da classe
